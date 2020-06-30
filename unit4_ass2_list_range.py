90781bad1d012658c928ac271201d42bf6b5773e
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-30 13:03:54 +0530

    Add files via upload

---
+++ unit4_ass2_list_range.py
@@ -0,0 +1,55 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "11 is in the range\n",
+      "None\n"
+     ]
+    }
+   ],
+   "source": [
+    "def var_range(n):\n",
+    "    if n in range(10,20):\n",
+    "        print('%s is in the range'%n)\n",
+    "    else :\n",
+    "        print(\"The number is not in range.\")\n",
+    "print(var_range(11))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
