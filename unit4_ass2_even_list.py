1a8555f2f04009d963f824d8503aa9987cb71cf7
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-30 12:56:26 +0530

    Add files via upload

---
+++ unit4_ass2_even_list.py
@@ -0,0 +1,51 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Even numbers in the list:  [10, 4, 66]\n"
+     ]
+    }
+   ],
+   "source": [
+    "list1 = [10, 21, 4, 45, 66, 93]  \n",
+    "even_nos = [num for num in list1 if num % 2 == 0] \n",
+    "print(\"Even numbers in the list: \", even_nos)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
